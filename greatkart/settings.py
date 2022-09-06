fe3318f87cf25526c6b25518426d5949f461daf4
Bruno Koura <brunokoura@gmail.com>
2022-09-06 14:22:40 -0300

    added categories

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
+#media files config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
