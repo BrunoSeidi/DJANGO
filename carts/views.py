b6995952396cb95eb13734f55494a9dc3d1abab0
Bruno Koura <brunokoura@gmail.com>
2022-09-13 14:29:14 -0300

    study does not exist

--- carts/views.py
+++ carts/views.py
@@ -37,5 +37,14 @@ def add_cart(request, product_id):
 
 
 
-def cart(request):
+def cart(request, total = 0, quantity = 0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except CartItem.DoesNotExist:
+        pass
+
     return render(request, 'store/cart.html')
